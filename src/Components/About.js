c9f0d4d2d91f7cddca859c5c00338a5c5cdbfbb6
Shams Reza <sumsrzza22@gmail.com>
2023-01-17 15:09:04 +0530

    Project is ready for deployment version 1.0

---
+++ src/Components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1>This Website is Developed by Shams Reza</h1>
+      <h2>Please Contact me at <a href = "mailto: abc@example.com">Email</a></h2>
+    </div>
+  )
+}
+
+export default About
